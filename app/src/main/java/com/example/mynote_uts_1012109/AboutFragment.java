5cb683e533cf0ccba02ffee1819e9beb5c9a1cc9
MaulanaAtapukan <maulanaatapukan@gmail.com>
2023-05-30 05:18:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mynote_uts_1012109/AboutFragment.java
@@ -0,0 +1,34 @@
+package com.example.mynote_uts_1012109;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//Nama  : Maulana Ardiansyah Atapukan
+//NIM   : 10120109
+//Kelas : IF-3
+
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/mynote_uts_1012109/HomeFragment.java
@@ -0,0 +1,75 @@
+package com.example.mynote_uts_1012109;
+
+//Nama  : Maulana Ardiansyah Atapukan
+//NIM   : 10120109
+//Kelas : IF-3
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+
+public class HomeFragment extends Fragment {
+    Toolbar toolbar;
+    private RecyclerView recyclerView;
+    FloatingActionButton button;
+    IAdapter adapter;
+    List<Note> notes;
+
+    public HomeFragment() {
+        // Required empty public constructor
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.listOfNote);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        button = view.findViewById(R.id.addBtn);
+        toolbar = view.findViewById(R.id.toolbar);
+        NoteDatabase db = new NoteDatabase(view.getContext());
+        notes = db.getNotes();
+        adapter = new IAdapter(view.getContext(),notes);
+        recyclerView.setAdapter(adapter);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),AddNoteActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
+
+    }
+
+}
